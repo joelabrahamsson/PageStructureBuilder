4ce5372376548010840df43f14b075166e7d034f
Tim Abell <tim@timwise.co.uk>
2012-03-23 11:39:21 +0000

    Code formatting of PageTypeStructureBase

--- PageStructureBuilder/PageTypeStructureBase.cs
+++ PageStructureBuilder/PageTypeStructureBase.cs
@@ -8,15 +8,13 @@ namespace PageStructureBuilder
     /// named with the first letter of the page name.
     /// </summary>
     /// <typeparam name="TContainer">The type page that will provide page type based folders.</typeparam>
-public abstract class PageTypeStructureBase<TContainer> : 
-    SingleLevelStructureBase<TContainer>
-    where TContainer : PageData
-{
-    protected override string GetContainerPageName(
-        PageData childPage)
+    public abstract class PageTypeStructureBase<TContainer> : SingleLevelStructureBase<TContainer>
+        where TContainer : PageData
     {
-        var pageType = PageType.Load(childPage.PageTypeID);
-        return pageType.Name;
+        protected override string GetContainerPageName(PageData childPage)
+        {
+            var pageType = PageType.Load(childPage.PageTypeID);
+            return pageType.Name;
+        }
     }
-}
 }
