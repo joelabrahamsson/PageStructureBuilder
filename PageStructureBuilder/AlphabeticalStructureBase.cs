3ade421470bbe810e657b5e49a9de251bc5a7f58
Tim Abell <tim@timwise.co.uk>
2012-03-23 11:36:34 +0000

    Code doc for AlphabeticalStructureBase

--- PageStructureBuilder/AlphabeticalStructureBase.cs
+++ PageStructureBuilder/AlphabeticalStructureBase.cs
@@ -2,6 +2,11 @@
 
 namespace PageStructureBuilder
 {
+    /// <summary>
+    /// Inherit a page type from this to automatically organise pages into folders
+    /// named with the first letter of the page name.
+    /// </summary>
+    /// <typeparam name="TCharacter">The type page that will provide single character folders.</typeparam>
     public abstract class AlphabeticalStructureBase<TCharacter> 
         : SingleLevelStructureBase<TCharacter>
         where TCharacter : PageData, new()
