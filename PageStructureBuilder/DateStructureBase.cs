f2d45ca5ae579f69e049fd497a3cfb638c84fafb
Tim Abell <tim@timwise.co.uk>
2012-03-23 10:02:48 +0000

    code doc for class DateStructureBase

--- PageStructureBuilder/DateStructureBase.cs
+++ PageStructureBuilder/DateStructureBase.cs
@@ -4,6 +4,23 @@ using PageTypeBuilder;
 
 namespace PageStructureBuilder
 {
+    /// <summary>
+    /// Inherit from this page type to make your pages automatically
+    /// move themselves to the date based folder structure of yyyy/mm/dd/pagename<br/>
+    /// The default date source is creation date. To change this override <see cref="GetStructureDate"/>.
+    /// </summary>
+    /// <typeparam name="TYear">Desired PageData type for year level container pages.</typeparam>
+    /// <typeparam name="TMonth">Desired PageData type for month level container pages.</typeparam>
+    /// <typeparam name="TDay">Desired PageData type for day level container pages.</typeparam>
+    /// <example>Usage: <code>
+    /// [PageType]
+    /// public class Container : TypedPageData { }
+    /// [PageType]
+    /// public class DateStructure : DateStructureBase&lt;Container, Container, Container&gt; { }
+    /// </code></example>
+    /// <remarks>
+    /// See also <a href="http://joelabrahamsson.com/entry/automatically-organize-episerver-pages-part-2/">http://joelabrahamsson.com/entry/automatically-organize-episerver-pages-part-2/</a>
+    /// </remarks>
     public abstract class DateStructureBase<TYear, TMonth, TDay> 
         : TypedPageData, IOrganizeChildren
         where TYear : PageData 
@@ -28,6 +45,13 @@ namespace PageStructureBuilder
             return dayPage.PageLink;
         }
 
+        /// <summary>
+        /// Returns the date that will be used when creating the page's folder
+        /// hierarchy. Default implementation uses the Created property of the page.<br/>
+        /// Override this method to use a different date for the hierarchy.
+        /// </summary>
+        /// <param name="pageToGetParentFor">The page to get parent for.</param>
+        /// <returns></returns>
         protected virtual DateTime GetStructureDate(
             PageData pageToGetParentFor)
         {
