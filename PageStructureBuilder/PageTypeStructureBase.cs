32e0b39366d229a31481488e30328dd76b3a0e93
Tim Abell <tim@timwise.co.uk>
2012-03-23 11:38:51 +0000

    Code doc for PageTypeStructureBase

--- PageStructureBuilder/PageTypeStructureBase.cs
+++ PageStructureBuilder/PageTypeStructureBase.cs
@@ -3,6 +3,11 @@ using EPiServer.DataAbstraction;
 
 namespace PageStructureBuilder
 {
+    /// <summary>
+    /// Inherit a page type from this to automatically organise pages into folders
+    /// named with the first letter of the page name.
+    /// </summary>
+    /// <typeparam name="TContainer">The type page that will provide page type based folders.</typeparam>
 public abstract class PageTypeStructureBase<TContainer> : 
     SingleLevelStructureBase<TContainer>
     where TContainer : PageData
