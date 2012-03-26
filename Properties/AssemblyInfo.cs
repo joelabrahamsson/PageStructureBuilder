858a6d3ef2b2b1df7a3f84fe5a13631c754bbc77
Tim Abell <tim@timwise.co.uk>
2012-03-26 10:02:37 +0100

    dll version => 1.1

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("PageStructureBuilder")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("https://github.com/timabell/PageStructureBuilder")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyProduct("PageStructureBuilder")]
 [assembly: AssemblyCopyright("Copyright © Joel Abrahamsson")]
@@ -31,4 +31,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
